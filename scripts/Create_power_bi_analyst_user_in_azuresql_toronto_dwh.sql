cac177199e1bdbecbfa573a17e6e3de67a25197e
uminskib <53011316+uminskib@users.noreply.github.com>
2024-11-12 21:11:45 +0100

    Update Create_power_bi_analyst_user_in_azuresql_toronto_dwh.sql

--- scripts/Create_power_bi_analyst_user_in_azuresql_toronto_dwh.sql
+++ scripts/Create_power_bi_analyst_user_in_azuresql_toronto_dwh.sql
@@ -1,4 +1,4 @@
 BEGIN
-CREATE USER powerbi_analyst_user WITH PASSWORD = 'b4083b@a662d94e6?ca7/bf3474f2e1b344';
+CREATE USER powerbi_analyst_user WITH PASSWORD = 'DEFINE YOUR OWN PASSWORD FOR USER';
 ALTER ROLE db_datareader ADD MEMBER powerbi_analyst_user;
 END
